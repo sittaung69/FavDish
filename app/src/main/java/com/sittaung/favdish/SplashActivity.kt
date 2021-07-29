cb80225d0f4db81bb764cc4f7393255793a9ac19
Sitt Aung <sittaung2021@gmail.com>
2021-07-29 19:26:28 +0630

    Adding animation to the splash screen

--- app/src/main/java/com/sittaung/favdish/SplashActivity.kt
+++ app/src/main/java/com/sittaung/favdish/SplashActivity.kt
@@ -1,6 +1,14 @@
 package com.sittaung.favdish
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.sittaung.favdish.databinding.ActivitySplashBinding
 
@@ -13,6 +21,35 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvAppName.text = "Hello Kotlin!"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        binding.tvAppName.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                //
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }, 1000)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                //
+            }
+
+        })
     }
 }
