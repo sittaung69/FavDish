e4c197e14b6e0767bc183d1f9d58c80d98400a3e
Sitt Aung <sittaung2021@gmail.com>
2021-07-30 10:23:36 +0630

    Designing the AddUpldateDishActivity

--- app/src/main/java/com/sittaung/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/sittaung/favdish/view/activities/AddUpdateDishActivity.kt
@@ -2,11 +2,42 @@ package com.sittaung.favdish.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.sittaung.favdish.R
+import com.sittaung.favdish.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        binding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupActionBar()
+
+        binding.ivAddDishImage.setOnClickListener(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_dish_image -> {
+                    Toast.makeText(this, "You have clicked the ImageView", Toast.LENGTH_SHORT)
+                        .show()
+                    return
+                }
+            }
+        }
     }
 }
