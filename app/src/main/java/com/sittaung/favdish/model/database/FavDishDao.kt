48f54ef7654374a3c5335970fd6e5952d448bdfe
Sitt Aung <sittaung2021@gmail.com>
2021-07-31 17:43:42 +0630

    Retrieving the inserted data from the database

--- app/src/main/java/com/sittaung/favdish/model/database/FavDishDao.kt
+++ app/src/main/java/com/sittaung/favdish/model/database/FavDishDao.kt
@@ -2,11 +2,16 @@ package com.sittaung.favdish.model.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.sittaung.favdish.model.entities.FavDish
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavDishDao {
 
     @Insert
     suspend fun insertFavDishDetails(favDish: FavDish)
+
+    @Query("SELECT * FROM fav_dishes_table ORDER BY ID")
+    fun getAllDishesList(): Flow<List<FavDish>>
 }
--- app/src/main/java/com/sittaung/favdish/view/fragments/AllDishesFragment.kt
+++ app/src/main/java/com/sittaung/favdish/view/fragments/AllDishesFragment.kt
@@ -2,19 +2,29 @@ package com.sittaung.favdish.view.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sittaung.favdish.R
+import com.sittaung.favdish.application.FavDishApplication
 import com.sittaung.favdish.databinding.FragmentAllDishesBinding
 import com.sittaung.favdish.view.activities.AddUpdateDishActivity
+import com.sittaung.favdish.viewmodel.FavDishViewModel
+import com.sittaung.favdish.viewmodel.FavDishViewModelFactory
 import com.sittaung.favdish.viewmodel.HomeViewModel
 
 class AllDishesFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    private val mFavDishViewModel: FavDishViewModel by viewModels {
+        FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+    }
+
     private var _binding: FragmentAllDishesBinding? = null
 
     // This property is only valid between onCreateView and
@@ -42,6 +52,18 @@ class AllDishesFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mFavDishViewModel.allDishesList.observe(viewLifecycleOwner) { dishes ->
+            dishes.let {
+                for (item in it) {
+                    Log.i("Dish Title", "${item.id} :: ${item.title}")
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
